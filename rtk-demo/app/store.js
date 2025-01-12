e389b1e3a4cc7b44bdb4d6e023a1057752c75519
Kajol sutra dhar <kajol.sd777@gmail.com>
2025-01-12 03:06:32 +0200

    Redux toolkit middlewares

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,12 +1,16 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
+const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const iceCreameReducer = require("../features/icecreame/iceCreamSlice");
+
+const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCreame: iceCreameReducer,
   },
+  middleware: (getMiddleware) => getMiddleware().concat(logger),
 });
 
 module.exports = store;
